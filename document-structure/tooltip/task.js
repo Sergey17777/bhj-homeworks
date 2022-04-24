3fcd4cbd3204494f455af583ccf18053115909ee
Сергей Григорьев <92942323+Sergey17777@users.noreply.github.com>
2022-04-24 12:57:44 +0500

    Update task.js

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,19 +1,51 @@
-const elementHasHint = document.querySelectorAll('.has-tooltip');
+'use strict'
+let hasTooltip = document.querySelectorAll('.has-tooltip');
+let position = ['bottom', 'bottom', 'right', 'left', 'top', 'top'];
 
-const hintRemuval = () => {
-    if (document.querySelector('.tooltip_active')) {
-        document.querySelector('.tooltip_active').remove();
+[...hasTooltip].map((elem, index) => elem.setAttribute('data-position', position[index]));
+
+document.addEventListener('click', e => {
+    e.preventDefault();
+    let target = e.target;
+    let rect = target.getBoundingClientRect()
+    let position = target.dataset.position;
+    !document.querySelector('.tooltip_active') && (document.body.innerHTML += `<div class="tooltip tooltip_active"></div>`);
+    let tooltip = document.querySelector('.tooltip_active');
+
+    if (target.title) {
+        if (tooltip.textContent == target.title) {
+            document.body.removeChild(tooltip);
+        } else {
+            tooltip.textContent = target.title;
+            tooltip.style.top = `${getPosition(rect, position, tooltip).posY}px`;
+            tooltip.style.left = `${getPosition(rect, position, tooltip).posX}px`;
+        }
     }
+});
+
+function getPosition(rect, position, tooltip) {
+    let posX, posY;
+
+    if (position == 'top') {
+        posY = rect.top - tooltip.getBoundingClientRect().height;
+        posX = rect.left;
+    
+    } else if(position == 'right') {
+        posX = rect.right;
+        posY = rect.top;
+
+    } else if(position == 'left') {
+        posX = rect.left - tooltip.getBoundingClientRect().width;
+        posY = rect.top;
+
+    } else {
+        posY = rect.bottom;
+        posX = rect.left;
+    }
+    return {posX: posX, posY: posY}
 }
 
-elementHasHint.forEach(element => element.addEventListener('click', (ev) => {
-    ev.preventDefault();
-    hintRemuval();
-    const location = {left, bottom} = ev.target.getBoundingClientRect();
-    console.log(location);
-    ev.target.insertAdjacentHTML("afterEnd", 
-    `<div class="tooltip tooltip_active" style="position:absolute; top:${location.bottom + 5}px; left:${location.left}px">
-        ${ev.target.title}
-    </div>`);
-    setTimeout(hintRemuval, 3000);
-}));
+document.addEventListener('scroll', () => {
+    let tooltip = document.querySelector('.tooltip_active');
+    tooltip && document.body.removeChild(tooltip);
+})
