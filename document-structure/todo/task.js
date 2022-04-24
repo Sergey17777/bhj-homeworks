37d20c63e8c363e9349f323a0b8ee95741a330db
Сергей Григорьев <92942323+Sergey17777@users.noreply.github.com>
2022-04-24 12:52:25 +0500

    Update task.js

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,37 +1,36 @@
-const addTaskButton = document.getElementById('tasks__add');
-const inputTaskField = document.getElementById('task__input');
-const taskList =  document.getElementById('tasks__list');
+'use strict'
 
-function addTask(){
-    if (inputTaskField.value) {
-        taskList.insertAdjacentHTML('beforeEnd', 
-            `<div class="task"> 
-                <div class="task__title">
-                ${inputTaskField.value}
-                </div>
-                <a href="#" class="task__remove">&times;</a>
-            </div>`);
+let addBtn = document.getElementById('tasks__add');
+let input = document.getElementById('task__input');
+let taskList = document.getElementById('tasks__list');
+let storage = window.localStorage;
+
+storage.saveList && storage.saveList.split('|').map(value => addTask(value));
+
+addBtn.addEventListener('click', e => {
+    e.preventDefault();
+    addTask(input.value);
+    storage.saveList ? storage.saveList += `|${input.value}` : storage.setItem('saveList', input.value);
+    input.value = '';
+});
+
+function addTask(value) {
+    if (value) {
+        let newTask = document.createElement('div');
+        newTask.classList.add('task');
+        newTask.innerHTML += `<div class="task__title">${value}</div>
+                                <a href="#" class="task__remove">&times;</a>`;
+        addListner(newTask);
+        taskList.appendChild(newTask);
     }
-    inputTaskField.value = null;
 }
 
-inputTaskField.addEventListener('keyup', (ev) => {
-    if (ev.key === 'Enter') {
-        addTask;
-    }
-})
-addTaskButton.addEventListener('click', (ev) => {
-    ev.preventDefault();
-    addTask();
-})
-
-taskList.addEventListener('click', (ev) => {
-    ev.preventDefault();
-    const deleteTaskButton = document.querySelectorAll('.task__remove');
-    deleteTaskButton.forEach(element => {
-        if (element === ev.target) {
-            element.parentElement.remove();
-        }
-    });
-}) 
-
+function addListner(task) {
+    let removeTask = task.querySelector('.task__remove')
+    removeTask.addEventListener('click', e => {
+        e.preventDefault();
+        taskList.removeChild(e.target.parentElement);
+        storage.saveList = storage.saveList.replace(e.target.previousElementSibling.textContent, '');
+        storage.saveList = storage.saveList.replace('||', '|')
+    })
+}
