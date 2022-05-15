634019e07b502bdca6e1358073eef98dfc931ae5
Сергей Григорьев <92942323+Sergey17777@users.noreply.github.com>
2022-05-15 13:45:14 +0500

    Add files via upload

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,32 +1,11 @@
-const editor = document.getElementById('editor');
-const clearKey = document.getElementById('clearKey');
-const clearAll = document.getElementById('clearAll');
+let editor = document.getElementById("editor");
+let reset = document.getElementById("resetBtn");
+let text = localStorage.getItem("textEditor");
 
-editor.addEventListener('input', saveText);
-window.addEventListener('load', printTextFromLocalStorage);
-clearKey.addEventListener('click', clear);
-clearAll.addEventListener('click', clear);
+window.addEventListener("load", function() {
+  editor.value = text;
+});
 
-
-function saveText(event) {
-    localStorage.text = editor.value;
-}
-
-function printTextFromLocalStorage(event) {
-    if (localStorage.text) {
-        editor.value = localStorage.text;
-    }
-}
-
-function clear(event) {
-
-    event.preventDefault();
-
-    if (event.target === clearKey) {
-        localStorage.removeItem('text');
-        editor.value = '';
-    } else {
-        localStorage.clear();
-        editor.value = '';
-    }
-}
+window.addEventListener("beforeunload", function() {
+  localStorage.setItem("textEditor", editor.value);
+});
